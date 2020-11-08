71fec9f1e651704fdd9d03921d910e1ad905e731
younhong <younhong@kakao.com>
2020-11-08 20:26:38 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
-import AppRouter, { Router } from "./Router";
+import AppRouter from "components/Router";
+import React, { useState } from 'react';
+import { authService } from '../firebaseInstance';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <AppRouter />
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Auth = () => <span>Auth</span>;
+const Auth = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+    }
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <input type="text" name="email"
+                    placeholder="Email"
+                    onChange={onChange}
+                    required value={email}/>
+                <input type="password" name="password"
+                    placeholder="Password" 
+                    onChange={onChange}
+                    required value={password}/>
+                <input type="submit" value="Login" />
+            </form>
+            <div>
+                <button>Continue with Google</button>
+                <button>Continue with Github</button>
+            </div>
+        </div>
+    );
+};
 export default Auth;
